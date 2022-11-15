c5f00b6ce8fb1c42224ea47f9c99a69cd2f44606
AbhyanthP <AbhyanthP@CTAADPG038Q88>
2022-11-15 09:56:13 +0530

    Add project files.

---
+++ csharp-day1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System; //root namespace contains most of the classes
+
+namespace day1 //namespace is a keyword class are put inside this multiple classes can be put in this
+//here csharp-day1 is also a namespace because it will have many classes in it 
+//if there are multiple clases unser one namespace then all the classes run and a. exe file is created 
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcom to C#");
+            
+        }
+    }
+}
---
+++ csharp-day1/Program1.cs
@@ -0,0 +1,19 @@
+using System;
+namespace day1
+{
+    class Program1
+    {
+        static void Main()
+        {   //writeline goes to the next line write keeps the cursor in the smae line
+            Console.WriteLine("Hello from Prog2");
+            Console.Write("Hello from Prog2");
+            // int age;
+            // int p=0;
+            // age=p;
+            // Console.Write(age);-->works
+            // int s;
+            // Console.Writeline(s);-->error  as no value is assigned
+            
+        }
+    }   
+}
